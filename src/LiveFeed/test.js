3a41d5e54cda677c30f291a7f4ce355514eba3ee
vamshidhar199 <vamshiredmi@gmail.com>
2023-10-12 22:52:41 -0700

    Added message data upload

--- src/LiveFeed/test.js
+++ src/LiveFeed/test.js
@@ -4,11 +4,13 @@ import React, { useEffect, useState } from "react";
 import ROSLIB from "roslib";
 import cross from "./download.png";
 import tick from "./tick.png";
+import { Buffer } from "buffer";
 
 import AWS from "aws-sdk";
 
 const ImageDisplay = () => {
   const [imageSrc, setImageSrc] = useState("");
+  const [rosTopic, setRosTopic] = useState();
   const [loadingText, setLoadingText] = useState("Loading...");
   const [uploadQueue, setUploadQueue] = useState([]);
   const [isUploading, setIsUploading] = useState(false);
@@ -19,18 +21,20 @@ const ImageDisplay = () => {
     secretAccessKey: "SS56EIg0c7O5PD1U9SAcs/gXxp09oFE96KmMXpad",
   });
 
+  // Start of use effect
   useEffect(() => {
     const ros = new ROSLIB.Ros({
       url: "ws://10.0.0.243:9090",
     });
 
+    // Define ros topic to be commected
     const topic = new ROSLIB.Topic({
       ros: ros,
-      name: "/yolov5_compressed_image",
-      messageType: "sensor_msgs/CompressedImage",
+      name: "/yolov8_custom_image", //"/yolov5_compressed_image",
+      messageType: "yolo_custom_topic/CustomMsg", //"sensor_msgs/CompressedImage",
     });
 
-    // Setting loading texts sequentially
+    // This is for loading
     const loadingTexts = ["Loading...", "Initializing...", "Hold On..."];
     let currentTextIndex = 0;
     const loadingInterval = setInterval(() => {
@@ -40,8 +44,10 @@ const ImageDisplay = () => {
 
     // subscribe to the ros topic
     topic.subscribe((message) => {
-      if (message && message.data) {
-        setImageSrc(`data:image/jpg;base64,${message.data}`);
+      // console.log(message);
+      if (message && message.processed_image.data) {
+        setRosTopic(message);
+        setImageSrc(`data:image/jpg;base64,${message.processed_image.data}`);
         clearInterval(loadingInterval);
         setIsStreaming(true);
       }
@@ -52,15 +58,18 @@ const ImageDisplay = () => {
       clearInterval(loadingInterval);
       setIsStreaming(false);
     };
-  }, []);
+  }, []); //useeffect end
 
   // To handle image capture when the user clicks "Upload"
-  const handleCaptureImage = () => {
-    console.log("Adding image to upload queue", imageSrc);
-    if (imageSrc) {
+  const handleCaptureImage = (rosTopicToUpload) => {
+    console.log("Adding topic to upload queue", rosTopicToUpload);
+
+    if (rosTopicToUpload) {
       // Add the current image details to the upload queue
       const imageDetails = {
-        dataURL: imageSrc,
+        processedImageDataURL: rosTopicToUpload.processed_image.data,
+        rawImageDataURL: rosTopicToUpload.raw_image.data,
+        annotationText: rosTopicToUpload.detections,
         timestamp: Date.now(),
       };
       setUploadQueue((prevQueue) => [...prevQueue, imageDetails]);
@@ -74,10 +83,39 @@ const ImageDisplay = () => {
     }
 
     setIsUploading(true);
+    console.log("Uploading data to S3", isUploading);
 
     for (const imageDetails of uploadQueue) {
-      const { dataURL, timestamp } = imageDetails;
-      await uploadImageToS3(dataURL, timestamp);
+      const {
+        processedImageDataURL,
+        rawImageDataURL,
+        annotationText,
+        timestamp,
+      } = imageDetails;
+      // Uploading processed image with annotation drawn on it
+      await uploadImageToS3(
+        processedImageDataURL,
+        timestamp,
+        "ReportedProcessedImages",
+        "Image",
+        ""
+      );
+      // Uploading raw image with no annotations
+      await uploadImageToS3(
+        rawImageDataURL,
+        timestamp,
+        "ReportedRawImages",
+        "Image",
+        ""
+      );
+      await uploadImageToS3(
+        annotationText,
+        timestamp,
+        "ReportedAnnotations",
+        "Text",
+        annotationText
+      );
+
       // Remove the uploaded image from the queue
       setUploadQueue((prevQueue) =>
         prevQueue.filter((item) => item.timestamp !== timestamp)
@@ -87,19 +125,43 @@ const ImageDisplay = () => {
     setIsUploading(false);
   };
 
+  // Since Buffer is not present in react js defining custom function
+  const convertBase64 = (base64) => {
+    var binaryString = atob(base64);
+    var len = binaryString.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+  };
+
   // upload image to S3
-  const uploadImageToS3 = async (dataURL, timestamp) => {
-    const base64 = dataURL.split(",")[1]; // Get the Base64 string, removing the prefix
-    const buf = Buffer.from(base64, "base64"); // Create a buffer from the Base64 string
+  const uploadImageToS3 = async (
+    dataURL,
+    timestamp,
+    directory,
+    type,
+    annotationText
+  ) => {
+    const buf = type == "Image" ? convertBase64(dataURL) : "";
 
     // Create the S3 parameters
-    const data = {
-      Bucket: "masterprojectbucket",
-      Key: `ReportImages/${timestamp}.jpg`, // set the file name
-      Body: buf,
-      ContentEncoding: "base64",
-      ContentType: "image/jpeg", // set the correct MIME type
-    };
+    const data =
+      type == "Image"
+        ? {
+            Bucket: "masterprojectbucket",
+            Key: `${directory}/image_${timestamp}.jpg`, // set the file name ReportImages
+            Body: buf,
+            ContentEncoding: "base64",
+            ContentType: "image/jpeg", // set the correct MIME type
+          }
+        : {
+            Bucket: "masterprojectbucket",
+            Key: `${directory}/image_${timestamp}.txt`, // set the file name ReportImages
+            Body: annotationText,
+            ContentType: "text/plain", // set the correct MIME type
+          };
 
     // Upload the image to S3
     s3.putObject(data, (err, data) => {
@@ -146,7 +208,7 @@ const ImageDisplay = () => {
                   src={cross}
                   className="side-image-image"
                   onClick={() => {
-                    handleCaptureImage();
+                    handleCaptureImage(rosTopic);
                   }}
                 ></img>
                 <img src={tick} className="side-image-image"></img>
